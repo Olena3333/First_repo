d652e060f49d59a2511da2748d46dee5d799415e
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2025-09-28 20:59:39 +0200

    4

--- main.py
+++ main.py
@@ -30,3 +30,36 @@ cat["characteristics"]=["l","k"]
 age=cat.get("age")
 cat.update(info)
 print(cat)
+#parne   if
+x = int(input('Введіть число: '))
+if x % 2 == 0:
+    print("Число x є парним.")
+else:
+    print("Число x є непарним.")
+
+
+is_next = None
+num = int(input("Enter the number of points: "))
+if num>=83:
+    is_next=True
+else:
+    is_next=False
+
+
+#if elif else
+a = input('Введіть число')
+a = int(a)
+if a == 1:
+    print('Число дорівнює 1')
+elif a > 0:
+    print('Число додатне')
+else:
+    print("a <= 0")
+
+work_experience = int(input("Enter your full work experience in years: "))
+if work_experience<=1:
+    developer_type = "Junior"
+elif work_experience<=5:
+    developer_type = "Middle"
+else:
+     developer_type = "Senior"
