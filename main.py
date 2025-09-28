048a262db6d665ce0665dcac732325b5cd9ae8a1
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2025-09-28 20:39:04 +0200

    3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ area = length*width
 show = f"With width {width} and length {length} of the room, its area is equal to {area}"
 print (show)
 
-my_list = []
+my_list = []  #List
 my_list.insert(0,2024)
 my_list.insert(1,"Python")
 my_list.insert(2,3.12)
@@ -17,3 +17,16 @@ my_list.insert(0,"fdhfdhfd")
 print(my_list)
 my_list.reverse()
 print(my_list)
+data = {} #diktionaries
+data["year"]=2024
+data["lang"]='Python'
+data["version"]=3.12
+print(data)
+cat = {}
+info = {"status": "vaccinated", "breed": True}
+cat["nick"]="Simon"
+cat["age"]=7
+cat["characteristics"]=["l","k"]
+age=cat.get("age")
+cat.update(info)
+print(cat)
