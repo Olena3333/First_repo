250ea95691615955d9a6478b5f1ee11fc46ecf70
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2025-09-28 20:07:57 +0200

    2

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print("Hello world!")
+length = 2.75
+width = 1.75
+area = length*width
+show = f"With width {width} and length {length} of the room, its area is equal to {area}"
+print (show)
+
+my_list = []
+my_list.insert(0,2024)
+my_list.insert(1,"Python")
+my_list.insert(2,3.12)
+print(my_list)
+some_data = ['Python']
+my_list.extend(some_data)
+print (my_list)
+my_list.insert(0,"fdhfdhfd")
+print(my_list)
+my_list.reverse()
+print(my_list)
